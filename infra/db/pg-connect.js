ecd175657c9795fcb39fac250728cb21924bba8d
c4miloruiz <cgomez08@gmail.com>
2022-11-06 20:39:33 -0500

    Hexagonal Architecture

--- infra/db/pg-connect.js
+++ infra/db/pg-connect.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const Pool = require('pg').Pool
 exports.pool = new Pool({
-    user: process.env.PGUSER,
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
 })
